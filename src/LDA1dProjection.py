73a9e3e790fd277048ef6c087537826d6f8378bd
DDOU <ddou@connect.ust.hk>
2018-10-01 14:33:02 -0500

    finish KFold helper function

---
+++ src/LDA1dProjection.py
@@ -0,0 +1,89 @@
+import numpy as np
+import sys, os, math, matplotlib
+import argparse
+from helper import KFold
+
+def buildParser():
+	parser = argparse.ArgumentParser(
+		prog = __file__
+	)
+
+	parser.add_argument(
+		"-f", "--filename",
+		type = str,
+		required = True,
+		dest = "filename",
+		help = "Path to data file"
+	)
+
+	parser.add_argument(
+		"-n", "--num_crossval",
+		type = int,
+		required = True,
+		dest = "num_crossval",
+		help = "Number of splits"
+	)
+
+	parser.add_argument(
+		"-v", "--verbose",
+		action = "store_true",
+		dest = "verbose",
+		help = "Enable printing debug information"
+	)
+
+	return parser
+
+class LDA1dProjection():	
+	def __init__(self, alpha):
+		self.alpha = alpha
+
+	def sigmoid(x):
+  		return 1 / (1 + math.exp(-x))
+	
+	def train(self, X, y):
+		pass
+
+	def predict(self, X, y):
+
+		return error
+
+
+
+def main():
+	parser = buildParser()
+	args = parser.parse_args()
+
+	if not os.path.isfile(args.filename):
+		sys.exit(
+			"ERROR: File does not exist"
+		)
+
+	data = np.genfromtxt(args.filename, delimiter=',')
+
+	X = data[:,:-1]
+	y = data[:,-1]
+
+	
+	if os.path.basename(args.filename) == 'boston.csv':
+		print('Modify the target of boston dataset...')
+		print()
+
+		median = np.median(y)
+		y = (y < median).astype(int)
+
+	
+	kfold = KFold(args.num_crossval)
+	for train_index, test_index in kfold.split(y):
+
+		X_train = X[train_index]
+		y_train = X[train_index]
+		X_test = y[test_index]
+		y_test = y[test_index]
+
+		model = LDA1dProjection(1)
+		model.train(X_train, y_train)
+		error = model.predict(X_test, y_test)
+
+
+if __name__ == '__main__':
+	main()
--- src/helper.py
+++ src/helper.py
@@ -2,12 +2,35 @@ import numpy as np
 import math, sys
 
 class KFold():
-	def __init__(self, num_splits, train_percent):
-		self.num_splits = num_splits
-		self.train_percent = train_percent
+	def __init__(self, num_crossval, shuffle = False):
+		self.num_crossval = num_crossval
+		self.shuffle = shuffle
 
-	def split():
-		pass
+	def split(self, y):
+		index_all = np.arange(y.shape[0])
+		if (self.shuffle):
+			np.random.shuffle(index_all)
+
+		test_index_num = math.floor(index_all.size/self.num_crossval)
+
+		reminder = index_all.size - test_index_num*self.num_crossval
+		
+		for i in range(self.num_crossval):
+
+			print('The',i+1,'fold of K-Fold cross-validation')
+			print()
+
+			if(i < reminder):
+				test_index = index_all[i*test_index_num+i:(i+1)*test_index_num+i+1]
+			else:
+				test_index = index_all[i*test_index_num+reminder:(i+1)*test_index_num+reminder]
+
+			train_index = np.setdiff1d(index_all, test_index, assume_unique=True)
+			
+			yield train_index, test_index
+
+
+
 
 class randomSplit():
 	def __init__(self, num_splits, train_percent):
@@ -22,7 +45,7 @@ class randomSplit():
 
 		for i in range(labels.size):
 			label_index = index_all[np.where(y==labels[i])]
-			temp = np.random.choice(label_index, math.ceil(label_index.size * percent), replace=False)
+			temp = np.random.choice(label_index, math.floor(label_index.size * percent), replace=False)
 			train_index = np.concatenate((train_index, temp))
 
 		test_index = np.setdiff1d(index_all, train_index, assume_unique=True)
@@ -33,7 +56,7 @@ class randomSplit():
 
 		for i in range(self.num_splits):
 
-			print('The',i+1,'split 10 random 80-20 train-test splits')
+			print('The',i+1,'split of 10 random 80-20 train-test splits')
 			print()
 
 			train, test_index = self.generateIndex(y, 0.8)
